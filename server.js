d28a04bff0531ef5415011146b5fec3cfaaf1270
Dhaval <dhaval.laiya@bacancytechnology.com>
2020-01-02 16:55:32 +0530

    Add promise and allow cors urls

--- server.js
+++ server.js
@@ -6,27 +6,44 @@ const { initMongo, ObjectId } = require('./server/MongoDB');
 const app = express();
 const port = process.env.PORT || 5000;
 var db;
+var allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
+var corsOptions = {
+  origin: (origin, callback) => {
+    // allow requests with no origin (mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Initialize connection once
-initMongo((dbObj) => {
-  if (!dbObj) {
-    return false;
-  }
-  db = dbObj;
-  app.listen(port, () => {
-    console.log('Server is running at port:', port);
-  });
-});
+initMongo // also can be done using callback function
+  .then((dbObj) => {
+    if (!dbObj) {
+      return false;
+    }
+    db = dbObj;
+    app.listen(port, () => {
+      console.log('Server is running at port:', port);
+    });
+  })
+  .catch(error =>
+    console.log(error));
 
 app.post('/api/add-note', (req, res) => {
   if (!req.body.note) {
     return res.send({
       error: true,
-      message: 'Please enter something.'
+      message: 'Please enter something to add.'
     })
   }
   db.collection('Notes').insertOne({
@@ -68,19 +85,22 @@ app.delete('/api/delete-note', (req, res) => {
       message: 'Please select id.'
     })
   }
-  db.collection('Notes').deleteOne({ _id: new ObjectId(req.body.id) }, (err, result) => {
-    if (err) {
-      return res.send({
-        error: true,
-        message: err
+
+  // return promise if callback is not passed
+  db.collection('Notes').deleteOne({ _id: new ObjectId(req.body.id) })
+    .then(result => {
+      res.send({
+        error: false,
+        message: 'Record has been deleted successfully.',
+        data: result
       })
-    }
-    return res.send({
-      error: false,
-      message: 'Record has been deleted successfully.',
-      data: result
     })
-  })
+    .catch(error => {
+      res.send({
+        error: true,
+        message: error
+      })
+    })
 });
 
 
--- server/MongoDB.js
+++ server/MongoDB.js
@@ -2,18 +2,18 @@ const { MongoClient, ObjectId } = require('mongodb');
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'NoteApp';
 
-const initMongo = (callback) => {
+const initMongo = new Promise((resolve, reject) => {
   // Initialize connection once
   MongoClient.connect(`${connectionUrl}/${dbName}`, { useUnifiedTopology: true }, (error, client) => {
     if (error) {
       console.log('Unable to connect to database.');
-      return callback();
+      reject(error);
     }
     console.log('Connected to database.');
     db = client.db(dbName);
-    return callback(db);
+    resolve(db);
   });
-}
+});
 
 module.exports = {
   initMongo,
